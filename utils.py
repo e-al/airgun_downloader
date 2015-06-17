86713dae44298d1c315fdb7bd1fa0ef9653fa94a
Alexey Zhikhartsev <last5bits@gmail.com>
2015-06-17 19:58:52 +0300

    Добавлена функция декодирования html-строк (&amp; => &)

---
+++ utils.py
@@ -0,0 +1,18 @@
+__author__ = 'last5bits'
+
+def html_decode(s):
+    """
+    Returns the ASCII decoded version of the given HTML string. This does
+    NOT remove normal HTML tags like <p>.
+    """
+    html_codes = (
+            ("'", '&#39;'),
+            ('"', '&quot;'),
+            ('>', '&gt;'),
+            ('<', '&lt;'),
+            ('&', '&amp;')
+        )
+
+    for code in html_codes:
+        s = s.replace(code[1], code[0])
+    return s
