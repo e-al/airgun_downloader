417427e12871f531043923db025d63e184d602e9
Alexey Zhikhartsev <last5bits@gmail.com>
2015-06-17 20:01:21 +0300

    Реализован вывод XML для категорий и подкатегорий

--- Category.py
+++ Category.py
@@ -1,11 +1,11 @@
 __author__ = 'last5bits'
 
-import warnings
 import urllib.request as req
 import re
 import os
 
 from Subcategory import Subcategory
+from utils import html_decode
 
 class Category:
     'Категория товаров: пистолет, винтовка и т.п.'
@@ -18,8 +18,11 @@ class Category:
         self.xml_str += '<category name="%s">' % self.get_name(html) + os.linesep
 
         # Добавить XML подкатегорий
-        for sub_url in self.get_subcategory_urls(html):
-            subcategory = Subcategory(sub_url)
+        for url_and_image in self.get_subcategory_urls_and_images(html):
+            prefix = 'http://www.air-gun.ru/'
+            url =  prefix + html_decode(url_and_image[0])
+            image = prefix + url_and_image[1]
+            subcategory = Subcategory(url, image)
             self.xml_str += subcategory.get_xml_str()
 
         self.xml_str += '</category>' + os.linesep
@@ -34,8 +37,14 @@ class Category:
         match = re.search('<title>(.+)<\/title>', str(html, "cp1251"))
         return match.group(1)
 
-    def get_subcategory_urls(self, html):
-        warnings.warn('Not implemented yet')
-        return html
+    def get_subcategory_urls_and_images(self, html):
+        'Возвращает список кортежей: URL страницы + URL картинки'
+
+        res = []
+        for match in re.finditer("<a href='(.+)'>\s*<img src='(images\/podcateg\/\d+.jpg)'", str(html, "cp1251")):
+            url, image = match.group(1), match.group(2)
+            res.append((url, image))
+
+        return res
 
     xml_str = ''
--- Subcategory.py
+++ Subcategory.py
@@ -1,16 +1,28 @@
 __author__ = 'e-al'
 
 import warnings
+import urllib.request as req
+import re
+import os
 
 class Subcategory:
     'Подкатегория товаров. Например, для пистолетов: Аникс, Umarex, Ижевск и т.д.'
 
-    def __init__(self, url):
-        warnings.warn('Not implemented yet')
-        pass
+    def __init__(self, url, img_url):
+        html = self.get_html(url)
+
+        self.xml_str += '<subcat name="%s" img="%s">' % (self.get_name(html), img_url) + os.linesep
+        self.xml_str += '</subcat>' + os.linesep
 
     def get_xml_str(self):
         warnings.warn('Not implemented yet')
         return self.xml_str
+
+    def get_html(self, url):
+        return req.urlopen(url).readall()
+
+    def get_name(self, html):
+        match = re.search('<title>(.+)<\/title>', str(html, "cp1251"))
+        return match.group(1)
 
     xml_str = ''
